379892509f27882cc81544e01933a609eb8e40cb
Eduardo Portela <eduardo.portela.jrr@gmail.com>
2023-01-05 10:51:42 -0300

    Feat: Create Page-routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { HomePage } from "./pages/homepage/homepage";
 import { GlobalStyle } from "./styles/globalStyles";
+import { RoutesMain as Routes } from "./routes/routes";
 
 export function App() {
   return (
     <div className="App">
       <GlobalStyle />
-      <HomePage />
+      <Routes />
     </div>
   );
 }
--- src/pages/register/register.tsx
+++ src/pages/register/register.tsx
@@ -1,1 +1,5 @@
-export {};
+import React from "react";
+
+export const Register = () => {
+  return <div>Rregister</div>;
+};
