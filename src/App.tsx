bfa565f024dcd29322824a1ae315ca786536d914
PauloHenriqueTCastro <106683665+PauloHenriqueTCastro@users.noreply.github.com>
2023-01-05 09:39:04 -0300

    NavRegister, ButtonsRegister, BackgroudRegister

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import React from "react";
+import { RegisterPage } from "./pages/register/register";
 import { GlobalStyle } from "./styles/globalStyles";
 
 export function App() {
   return (
     <div>
+      <RegisterPage></RegisterPage>
       <GlobalStyle />
     </div>
   );
--- src/pages/register/register.tsx
+++ src/pages/register/register.tsx
@@ -1,3 +1,25 @@
+import { NavRegister } from "../../components/NavRegister/NavRegister";
+import { RegisterButtonsCoteiner, RegisterConteiner } from "./registerStyle";
+import imgClientRegister from "./../../assets/img/clientRegisterButtonImg.png";
+import imgProviderRegister from "./../../assets/img/providerRegisterButtonImg.png";
+import { useState } from "react";
+
 export function RegisterPage() {
-  return;
+  const [showClientModal, setShowClientModal] = useState(false);
+
+  return (
+    <RegisterConteiner>
+      <NavRegister />
+      <RegisterButtonsCoteiner>
+        <div onClick={() => setShowClientModal(true)}>
+          <img src={imgClientRegister} alt="Cadastro do cliente" />
+          <p>Cadastro do cliente</p>
+        </div>
+        <div>
+          <img src={imgProviderRegister} alt="Cadastro do fornecedor" />
+          <p>Cadastro de fornecedor</p>
+        </div>
+      </RegisterButtonsCoteiner>
+    </RegisterConteiner>
+  );
 }
